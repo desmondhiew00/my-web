ab266b3b15e9ec5c3ff59740165909ee94f00ea5
Desmond Hiew <desmond.hiew@techiesapp.io>
2024-06-19 12:08:05 +0900

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,14 +26,20 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="description" content="Desmond Hiew's hello world." />
+				<meta name="description" content="desmondhiew's hello world." />
 				<meta name="theme-color" content="#000000" />
 				<meta name="color-scheme" content="light dark" />
+
+				<meta name="creator" content="desmondhiew" />
+				<meta name="author" content="desmondhiew" />
+				<meta property="og:title" content="desmondhiew" />
+				<meta property="og:description" content="desmondhiew's hello world" />
 				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Desmond Hiew" />
-				<meta property="og:description" content="Enter Desmond Hiew's hello world" />
 				<meta property="og:url" content="https://desmondhiew.com" />
-				<meta property="og:site_name" content="Desmond Hiew" />
+				<meta property="og:site_name" content="desmondhiew" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="desmondhiew" />
+				<meta name="twitter:description" content="desmondhiew's hello world" />
 			</head>
 
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
