5ee65f0953eea8d053fbf64b75fdf33f043687e0
Desmond Hiew <desmond.hiew@techiesapp.io>
2024-06-20 15:02:08 +0900

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
 	title: "desmondhiew",
-	description: "Desmond Hiew",
+	description: "Full-stack developer, making things works with code.",
 };
 
 export default function RootLayout({
@@ -25,15 +25,14 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<meta name="description" content="desmondhiew's hello world." />
+				<meta name="description" content={metadata.description || ""} />
 				<meta name="theme-color" content="#000000" />
 				<meta name="color-scheme" content="light dark" />
 
 				<meta name="creator" content="desmondhiew" />
 				<meta name="author" content="desmondhiew" />
 				<meta property="og:title" content="desmondhiew" />
-				<meta property="og:description" content="desmondhiew's hello world" />
+				<meta property="og:description" content={metadata.description || ""} />
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content="https://desmondhiew.com" />
 				<meta property="og:site_name" content="desmondhiew" />
